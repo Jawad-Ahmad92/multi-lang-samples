42a9cd7523434e07c27f5ba55f102f92172f3395
jawad-Ahmad92 <jawadahmad9296@gmail.com>
2025-11-18 21:28:35 +0500

    OR operrator in conditional folder

---
+++ Conditional/OR_operator.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+using namespace std;
+int main(){
+		char vovl;
+		
+		cout<<"enter a alphabet check its vavol are not";
+		cin>>vovl;
+		if(vovl=='a' || vovl=='e'|| vovl=='i' || vovl=='o' ||vovl=='u'){
+			cout<<"its a volvel";
+		}else
+		cout<<"invalid char";
+	return 0;
+}
