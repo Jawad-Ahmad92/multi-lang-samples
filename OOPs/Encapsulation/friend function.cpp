bd60a566b4f32a904feb8b71b3c6c3b77b783f9d
jawad-Ahmad92 <jawadahmad9296@gmail.com>
2025-11-20 17:36:31 +0500

    freind function

---
+++ OOPs/Encapsulation/friend function.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class text{
+	int x;
+	public:
+		
+		friend   int in(text &t);
+	    friend   int out(text &t);
+};
+int  in(text &t){
+	cout<<"enter value ";
+	cin>>t.x;
+}
+int out(text &t){
+	cout<<"x value is "<<t.x<<endl;
+}
+int main()
+{
+	text ob;
+	in(ob);
+	out(ob);
+	
+	return 0;
+}
