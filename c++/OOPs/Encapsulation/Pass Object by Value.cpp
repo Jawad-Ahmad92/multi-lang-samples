9642b2c8841dbcc63b78dedb46bbea9c0392e8af
jawad-Ahmad92 <jawadahmad9296@gmail.com>
2025-11-21 22:09:31 +0500

    pass object by value

---
+++ c++/OOPs/Encapsulation/Pass Object by Value.cpp
@@ -0,0 +1,32 @@
+/* Pass by Value
+void func(ClassName obj)
+Makes a copy of the object*/ 
+ #include<iostream>
+using namespace std;
+
+class Student {
+    int age;
+public:
+    void setAge(int a) {
+        age = a;
+    }
+
+    void showAge() {
+        cout << "Age = " << age << endl;
+    }
+
+    void display(Student s) {  // <-- object passed by value
+        cout << "Displaying age of another student: " << s.age << endl;
+    }
+};
+
+int main() {
+    Student s1, s2;
+    s1.setAge(20);
+    s2.setAge(25);
+
+    s1.display(s2);  // Passing object s2 to function display()
+
+    return 0;
+}
+
