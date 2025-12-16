3dba6d46dc832f2bc8d1716d8f4f133e6933bb70
jawad-Ahmad92 <jawadahmad9296@gmail.com>
2025-12-16 23:43:01 +0500

    add dictionary topic in python

---
+++ python/Dictionary/dictoinar.py
@@ -0,0 +1,11 @@
+#Dictionary-->it is used to store data values in key : value pairs 
+#they are unordered , mutaue (changebal) and dont allow duplicate key
+
+dict={
+    "name":"jawad ahmad",
+    "gpa": 3.5,
+    'marks':[78,87,90,87],   #we store list and tuples alway in dictionary
+}
+print(dict)  #they print all values
+print(dict["name"])  #they print only jawad ahmad
+print(dict["marks"])  #they print only [78,87,90,87]
